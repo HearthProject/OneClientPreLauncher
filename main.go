59cdb282e752eaa1b975d973f85115776c6b946b
modmuss50 <modmuss50@gmail.com>
2017-08-31 16:41:16 +0100

    Updates

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/HearthProject/OneClientPreLauncher/utils"
 	"github.com/shibukawa/configdir"
 	"path/filepath"
-	"github.com/go-toast/toast"
 	"os/exec"
 	"log"
 	"os"
@@ -14,17 +13,15 @@ import (
 
 
 func main(){
-	fmt.Println("Starting the oneclient!")
+	fmt.Println("Starting One Client!")
 
-
+	fmt.Println("Checking for new version!")
 	versionJson, err := utils.GetString("http://hearthproject.uk/files/versions.json")
 	if err != nil {
 		println(err)
 	}
 	downloadUrl := utils.GetStringValue(versionJson, "downloadUrl")
 	latestVersion := utils.GetStringValue(versionJson, "latestVersion")
-	fmt.Println(downloadUrl)
-	fmt.Println(latestVersion)
 
 	saveDir := configdir.New("hearthproject", "oneclientprelauncher").QueryCacheFolder().Path
 	if !utils.FileExists(saveDir){
@@ -34,14 +31,6 @@ func main(){
 	jarFile := saveDir + string(filepath.Separator) + latestVersion + ".jar"
 
 	if !utils.FileExists(jarFile){
-
-		notification := toast.Notification{
-			AppID: "One Client",
-			Title: "Downloading One Client update!",
-			Message: "OneClient is now being updated to version " + latestVersion,
-		}
-		notification.Push()
-
 		fmt.Println("Downloading launcher update to " + jarFile)
 		err := utils.DownloadFile(jarFile, downloadUrl)
 		if err != nil {
@@ -49,22 +38,13 @@ func main(){
 		}
 	}
 
-	if !IsJavaInstalled() {
-		//TODO show an error
-	}
-
-
+	fmt.Println("Starting...")
 	exec := exec.Command("java", "-jar", jarFile, "-native_launch")
 	exec.Dir = saveDir
-	er := exec.Run()
+	er := exec.Start()
 	if er != nil {
 		log.Fatal(er)
 		os.Exit(1)
 	}
-	fmt.Println("Goodbye")
 }
 
-func IsJavaInstalled() bool {
-	_, err := exec.LookPath("java")
-	return err != nil
-}
